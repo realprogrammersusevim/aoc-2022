be89e58c5eaae6de9092ffffd30d1273dbcd0061
Jonathan Milligan <logouts.deco-0e@icloud.com>
2022-12-03 23:08:40 -0600

    feature: Complete Day 2 of the Advent of Code
    I successfully completed the puzzle for Day 2 of the Advent of Code!

---
+++ src/day_2.rs
@@ -0,0 +1,82 @@
+use crate::utils::read_file;
+
+// Puzzle is true for first and false for second
+pub fn eval_strat(puzzle: bool) -> i32 {
+    let input = read_file("input/2.txt");
+    let lines: Vec<&str> = input.lines().collect();
+    let mut you: Vec<&str> = Vec::new();
+    let mut them: Vec<&str> = Vec::new();
+
+    for line in lines.iter() {
+        // Push the first char
+        them.push(&line[0..1]);
+        you.push(&line[2..3]);
+    }
+
+    assert_eq!(you.len(), them.len()); // As they say, fail fast if something's wrong
+
+    let mut total_points = 0;
+
+    if puzzle {
+        for i in 0..you.len() {
+            let you_char = you[i];
+            let them_char = them[i];
+
+            total_points += winner(them_char, you_char)
+        }
+    } else {
+        for i in 0..you.len() {
+            let you_char = play(them[i], you[i]);
+            let them_char = them[i];
+
+            total_points += winner(them_char, you_char)
+        }
+    }
+
+    return total_points;
+}
+
+fn winner(them: &str, you: &str) -> i32 {
+    let chars: (&str, &str) = (them, you);
+    // A is Rock
+    // B is Paper
+    // C is Scissors
+    // X is Rock
+    // Y is Paper
+    // Z is Scissors
+    // Return 6 if win, 3 if tie, 0 if loss plus 1 for rock, 2 for paper, and 3 for scissors
+    match chars {
+        ("A", "X") => 4,
+        ("A", "Y") => 8,
+        ("A", "Z") => 3,
+        ("B", "X") => 1,
+        ("B", "Y") => 5,
+        ("B", "Z") => 9,
+        ("C", "X") => 7,
+        ("C", "Y") => 2,
+        ("C", "Z") => 6,
+        _ => 0,
+    }
+}
+
+fn play(play: &str, outcome: &str) -> &'static str {
+    let chars: (&str, &str) = (play, outcome);
+    // The first char is what they played and the second char is the outcome you need to achieve
+    // X means you need to lose
+    // Y means you need to tie
+    // Z means you need to win
+    // Return the move you need to make
+
+    match chars {
+        ("A", "X") => "Z",
+        ("A", "Y") => "X",
+        ("A", "Z") => "Y",
+        ("B", "X") => "X",
+        ("B", "Y") => "Y",
+        ("B", "Z") => "Z",
+        ("C", "X") => "Y",
+        ("C", "Y") => "Z",
+        ("C", "Z") => "X",
+        _ => "X",
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 mod day_1;
+mod day_2;
 mod utils;
 
 fn main() {
-    let elf_calories = day_1::get_calories();
-    println!("The elf carrying the most calories has {}, the second has {}, the third has {}, and the sum of those top three is {}", elf_calories[0], elf_calories[1], elf_calories[2], elf_calories[3])
+    // Day 1
+    // let elf_calories = day_1::get_calories();
+    // println!("The elf carrying the most calories has {}, the second has {}, the third has {}, and the sum of those top three is {}", elf_calories[0], elf_calories[1], elf_calories[2], elf_calories[3]);
+
+    // Day 2
+    // println!("Total points: {}", day_2::eval_strat(true)); // For puzzle one
+    println!("Total points: {}", day_2::eval_strat(false)); // For puzzle two
 }
